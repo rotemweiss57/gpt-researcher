1ed86db4585c1cec87c67804a0e751fae43b744f
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-07-14 02:26:53 -0400

    chromedriver update

--- actions/web_scrape.py
+++ actions/web_scrape.py
@@ -131,14 +131,14 @@ def scrape_text_with_selenium(url: str) -> tuple[WebDriver, str]:
         driver = webdriver.Safari(options=options)
     else:
         if platform == "linux" or platform == "linux2":
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--remote-debugging-port=9222")
+            #options.add_argument("--disable-dev-shm-usage")
+            #options.add_argument("--remote-debugging-port=9222")
             options.add_argument("--headless")
             options.add_argument("window-size=1400,1500")
-            options.add_argument("--disable-gpu")
-            options.add_argument("start-maximized")
-            options.add_argument("enable-automation")
-            options.add_argument("--disable-infobars")
+            #options.add_argument("--disable-gpu")
+            #options.add_argument("start-maximized")
+            #options.add_argument("enable-automation")
+            #options.add_argument("--disable-infobars")
         options.add_argument("--no-sandbox")
         service = Service(executable_path=ChromeDriverManager().install())
         driver = webdriver.Chrome(
