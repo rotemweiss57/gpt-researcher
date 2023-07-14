4312a78043fa5d9d5b13a978032f261bd6a2f921
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-07-13 22:11:51 -0400

    test

--- .platform/hooks/predeploy/01_weasyprint.sh
+++ .platform/hooks/predeploy/01_weasyprint.sh
@@ -64,3 +64,13 @@ sudo make install
 cd ..
 
 sudo ldconfig /usr/lib
+
+cd /tmp/
+sudo wget https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+chromedriver --version
+
+sudo curl https://intoli.com/install-google-chrome.sh | bash
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+google-chrome --version && which google-chrome
