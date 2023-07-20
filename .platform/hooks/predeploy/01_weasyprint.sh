85f49dad024912b1f8cb1dbeb7f5c17187b6e3d0
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-07-20 16:01:39 -0400

    fix chrome driver

--- .platform/hooks/predeploy/01_weasyprint.sh
+++ .platform/hooks/predeploy/01_weasyprint.sh
@@ -88,16 +88,14 @@ fi
 
 sudo ldconfig /usr/lib
 
+
 cd /tmp/
-wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip
 sudo mv -f chromedriver /usr/bin/chromedriver
 chromedriver --version
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
+sudo curl https://intoli.com/install-google-chrome.sh | bash
 sudo mv -f /usr/bin/google-chrome-stable /usr/bin/google-chrome
 google-chrome --version && which google-chrome
-
 
